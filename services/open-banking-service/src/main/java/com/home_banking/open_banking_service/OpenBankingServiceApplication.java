32ba8f41b39aa741a33117a9f011b1922df14fbc
02Kani <kanikaradag02@gmail.com>
2026-03-22 17:59:19 +0100

    adding service

---
+++ services/open-banking-service/src/main/java/com/home_banking/open_banking_service/OpenBankingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.home_banking.open_banking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenBankingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenBankingServiceApplication.class, args);
+	}
+
+}
---
+++ services/open-banking-service/src/test/java/com/home_banking/open_banking_service/OpenBankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home_banking.open_banking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenBankingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
