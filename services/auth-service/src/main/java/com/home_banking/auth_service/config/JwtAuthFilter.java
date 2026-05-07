8a9cc265cb973308e1b21b8b8b8678f96776c855
02Kani <kanikaradag02@gmail.com>
2026-05-07 19:31:20 +0200

    extend jwt auth filter method

--- services/auth-service/src/main/java/com/home_banking/auth_service/config/JwtAuthFilter.java
+++ services/auth-service/src/main/java/com/home_banking/auth_service/config/JwtAuthFilter.java
@@ -1,15 +1,16 @@
 package com.home_banking.auth_service.config;
-
-import com.home_banking.auth_service.entity.User;
 import com.home_banking.auth_service.service.IJwtService;
-import com.home_banking.auth_service.service.IUserDetailService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -25,7 +26,7 @@ This makes sure the filter checks the request for different methods all the time
 @RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
     private final IJwtService jwtService;
-    private final IUserDetailService userDetailService;
+    private final UserDetailsService userDetailService;
 
     @Override
     protected void doFilterInternal(
@@ -48,7 +49,16 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
         // SecurityContextHolder == null, shows that the user is not already authenticated: therefore skip the authentication process
         if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            User user = userDetailService.loadUserByUsername(userEmail);
+            UserDetails userDetails = userDetailService.loadUserByUsername(userEmail);
+            if(jwtService.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                // extend details of token with details of our request
+                token.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+            }
+            filterChain.doFilter(request, response);
         }
     }
 }
--- services/auth-service/src/main/java/com/home_banking/auth_service/service/IUserDetailService.java
+++
@@ -1,4 +0,0 @@
-package com.home_banking.auth_service.service;
-
-public interface IUserDetailService {
-}
