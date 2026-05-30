10dab2d6ef7ddebfd7dfe7f7bca8a16f080d3180
02Kani <kanikaradag02@gmail.com>
2026-05-30 21:05:45 +0200

    #

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -1,14 +1,25 @@
-export type Transaction = {
+export interface Transaction {
   id: number;
   accountId: string;
   amount: string;
   currency: string;
   creditorName: string;
   debtorName: string;
-  type: "CRDT" | "DBIT";
+  type: CreditDebitIndicator;
   bookingDate: string;
-  status: "BOOK" | "CNCL" | "HOLD" | "OTHR" | "PDNG" | "RJCT" | "SCHD";
+  status: TransactionStatus;
   categoryId: number;
   categoryName: string;
   categoryColor: string;
-};
+}
+
+export type TransactionStatus =
+  | "BOOK"
+  | "CNCL"
+  | "HOLD"
+  | "OTHR"
+  | "PDNG"
+  | "RJCT"
+  | "SCHD";
+
+export type CreditDebitIndicator = "CRDT" | "DBIT";
